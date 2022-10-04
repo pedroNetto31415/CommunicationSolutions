7493b226785828ff07e4a64592feea6204547ee8
pedroNetto31415 <pedronetto31415@gmail.com>
2022-10-04 17:12:57 -0300

    Criando Pastas de organização no Domain

---
+++ CommunicationSolutions.Domain/Model/Request/TWWebHookRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationSolutions.Domain.Model.Request
+{
+    internal class TWWebHookRequest
+    {
+    }
+}
